40a49b244c480da187c4285858934d8f8e9dcb5b
vili105 <vili105@gmail.com>
2015-03-03 22:14:36 +0200

    Undo

--- src/tests/LoginUserStory.java
+++ src/tests/LoginUserStory.java
@@ -1,8 +1,5 @@
 import static org.junit.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.lang3.StringUtils;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -18,7 +15,7 @@ public class LoginUserStory {
 	public void setUp()
 	{
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		driver.get(URL);
 	}
 	
@@ -101,39 +98,5 @@ public class LoginUserStory {
 		WebElement identity = driver.findElement(By.id("identity"));
 		
 		assertTrue(identity.getText().contains("vil vil"));
-	}
-	
-	// Test that longer emails than 128 characters will not be accepted
-	// an error message will be returned
-	@Test
-	public void Scenario5()
-	{
-		WebElement username_input = driver.findElement(By.id("session_key-login"));
-		username_input.sendKeys(StringUtils.repeat("a", 200));
-		WebElement password_input = driver.findElement(By.id("session_password-login"));
-		password_input.sendKeys("wrong_password");
-		WebElement form = driver.findElement(By.id("signin"));
-		form.submit();
-		
-		WebElement error = driver.findElement(By.id("session_key-login-error"));
-		
-		assertTrue(error.getText().contains("The text you provided is too long"));
-	}
-
-	// Test that longer passwords than 400 characters will not be accepted
-	// an error message will be returned
-	@Test
-	public void Scenario6()
-	{
-		WebElement username_input = driver.findElement(By.id("session_key-login"));
-		username_input.sendKeys("vili105@abv.bg");
-		WebElement password_input = driver.findElement(By.id("session_password-login"));
-		password_input.sendKeys(StringUtils.repeat("a", 500));
-		WebElement form = driver.findElement(By.id("signin"));
-		form.submit();
-		
-		WebElement error = driver.findElement(By.id("session_password-login-error"));
-		
-		assertEquals(error.getText(), "The password you provided must have at most 400 characters.");
 	}
 }
