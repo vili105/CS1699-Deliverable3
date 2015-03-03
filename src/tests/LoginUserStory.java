757aef10bf6b813deae80b8635db40e06b175201
vili105 <vili105@gmail.com>
2015-03-03 19:49:04 +0200

    Added 2 new scenarios to the LoginUserStory

--- src/tests/LoginUserStory.java
+++ src/tests/LoginUserStory.java
@@ -97,4 +97,42 @@ public class LoginUserStory {
 		
 		assertTrue(identity.getText().contains("vil vil"));
 	}
+	
+	// Longer emails than 128 characters should not be accepted
+	// Fill the email field with 200 repeating 'a' characters
+	// Check if the proper error message is displayed
+	@Test
+	public void Scenario5()
+	{
+		driver.get("https://www.linkedin.com/");
+		WebElement username_input = driver.findElement(By.id("session_key-login"));
+		username_input.sendKeys(StringUtils.repeat("a", 200));
+		WebElement password_input = driver.findElement(By.id("session_password-login"));
+		password_input.sendKeys("wrong_password");
+		WebElement form = driver.findElement(By.id("signin"));
+		form.submit();
+		
+		WebElement error = driver.findElement(By.id("session_key-login-error"));
+		
+		assertTrue(error.getText().contains("The text you provided is too long"));
+	}
+
+	// Longer passwords than 400 characters should not be accepted
+	// Fill the password field with 500 repeating 'a' characters
+	// Check if the proper error message is displayed
+	@Test
+	public void Scenario6()
+	{
+		driver.get("https://www.linkedin.com/");
+		WebElement username_input = driver.findElement(By.id("session_key-login"));
+		username_input.sendKeys("vili105@abv.bg");
+		WebElement password_input = driver.findElement(By.id("session_password-login"));
+		password_input.sendKeys(StringUtils.repeat("a", 500));
+		WebElement form = driver.findElement(By.id("signin"));
+		form.submit();
+		
+		WebElement error = driver.findElement(By.id("session_password-login-error"));
+		
+		assertEquals(error.getText(), "The password you provided must have at most 400 characters.");
+	}
 }
